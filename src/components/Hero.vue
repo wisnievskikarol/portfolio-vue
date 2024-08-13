d584e61429ae119b92f20e9f71cc2de2166eeb07
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-13 11:51:25 +0200

    init project

---
+++ src/components/Hero.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">Hello! I'm Karol Wisniewski</h1>
+    <h3>
+      Frontend developer
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// import TheWelcome from '../components/TheWelcome.vue'
+import Hero from '../components/Hero.vue'
+</script>
+
+<template>
+  <main>
+    <Hero />
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
