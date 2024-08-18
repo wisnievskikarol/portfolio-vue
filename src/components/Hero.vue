1fd90a524383700335efb3c6418a238300f1a830
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-18 14:02:52 +0200

    chore(Hero): clean up component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts" />
 
 <template>
-  <div class="greetings">
-    <h1 class="green">Hello! I'm Karol Wisniewski</h1>
-    <h3>
-      Frontend developer
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
-  </div>
+  <header>
+    <h1>Hello! I'm Karol Wisniewski</h1>
+    <h3>Frontend developer</h3>
+  </header>
 </template>
