80afba6e9a5a6ed066b985d61bed649bfe2bc8b9
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-18 13:52:03 +0200

    feat: add Section

---
+++ src/components/common/Section.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ title: string }>()
+</script>
+
+<template>
+  <div class="flex gap-4 flex-col">
+    <h2 class="uppercase text-lightGray font-normal text-lg tracking-widest">{{ title }}</h2>
+    <div class="px-2 sm:px-4">
+      <slot></slot>
+    </div>
+  </div>
+</template>
