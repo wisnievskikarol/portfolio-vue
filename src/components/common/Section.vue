42c9be8f70408d32cdfaaee0b5c5640e23c4b46c
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-18 21:52:44 +0200

    feat(Section): pass attrs

--- src/components/common/Section.vue
+++ src/components/common/Section.vue
@@ -3,7 +3,7 @@ defineProps<{ title: string }>()
 </script>
 
 <template>
-  <div class="flex gap-4 flex-col">
+  <div v-bind="$attrs" class="flex gap-4 flex-col">
     <h2 class="uppercase text-lightGray font-normal text-lg tracking-widest">{{ title }}</h2>
     <div class="px-2 sm:px-4">
       <slot></slot>
