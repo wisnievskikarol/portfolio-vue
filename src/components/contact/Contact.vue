b3599c0f5606d26e57af7746b9db5602ae40ac09
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-18 21:51:35 +0200

    feat: add Contact

---
+++ src/components/contact/Contact.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Section from '@/components/common/Section.vue'
+</script>
+
+<template>
+  <Section id="contact" title="📝 contact"
+    ><ul class="text-darkGray">
+      <a
+        class="transition delay-75 duration-300 ease-in-out hover:text-primary text-lg sm:text-2xl underline"
+        href="tel:+48-737-909-932"
+      >
+        <p>+48 737 909 932</p>
+      </a>
+      <a
+        class="transition delay-75 duration-300 ease-in-out hover:text-primary text-lg sm:text-2xl underline"
+        href="mailto:wisniewskikarol@gmail.com"
+        >wisniewskikarol@gmail.com</a
+      >
+    </ul>
+  </Section>
+</template>
