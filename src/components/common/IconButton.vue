06d64de85eeaa133ad142da623456b68fe906db5
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-18 15:41:58 +0200

    chore(IconButton): remove unused RouterLink

--- src/components/common/IconButton.vue
+++ src/components/common/IconButton.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type { IconButtonProps } from './types'
-import { RouterLink } from 'vue-router'
 import { ArrowUpRightIcon } from '@heroicons/vue/24/solid'
 
 const props = withDefaults(defineProps<IconButtonProps>(), {
@@ -14,16 +13,14 @@ const sizeClass = size === 'm' ? 'text-lg' : 'text-sm'
 
 <template>
   <div class="text-lightGray transition delay-100 duration-300 ease-in-out hover:text-primary">
-    <RouterLink v-if="to" :to="to">
-      <button>{{ label }}</button>
-    </RouterLink>
-    <button
-      :class="sizeClass"
-      class="bg-veryDarkGray flex gap-2 justify-center content-center w-min py-1 px-4 rounded-full"
-      v-else
-    >
-      {{ label }}
-      <ArrowUpRightIcon class="w-3 h-4" />
-    </button>
+    <a :href="to" target="_blank">
+      <button
+        :class="sizeClass"
+        class="bg-veryDarkGray flex gap-2 justify-center content-center w-min py-1 px-4 rounded-full"
+      >
+        {{ label }}
+        <ArrowUpRightIcon class="w-3 h-4" />
+      </button>
+    </a>
   </div>
 </template>
