66f2a73202da8e3c88eea08cdd4585e6f90ff548
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-18 21:50:19 +0200

    feat: add Hero

--- src/components/Hero.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts" />
-
-<template>
-  <header>
-    <h1>Hello! I'm Karol Wisniewski</h1>
-    <h3>Frontend developer</h3>
-  </header>
-</template>
---
+++ src/components/hero/Hero.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import KarolWisniewskiCV from '@/assets/KarolWisniewski_CV.pdf'
+</script>
+
+<template>
+  <div class="flex gap-6 py-10 sm:py-20 flex-col justify-start">
+    <div class="flex flex-col sm:flex-row content-center">
+      <div class="text-lightGray text-3xl font-normal">
+        <h1 class="mb-2">Hey 👋</h1>
+        <h1
+          class="mb-4 font-normal bg-gradient-to-r from-primary to-white inline-block text-transparent bg-clip-text"
+        >
+          I'm Karol, Front-end developer
+        </h1>
+        <h2 class="text-darkGray max-w-5xl leading-relaxed font-light mt-4 text-base">
+          As a front-end developer, I am driven by a passion for creating innovative, user-centric
+          web experiences that blend functionality with elegance.
+        </h2>
+      </div>
+    </div>
+    <div class="flex gap-2">
+      <IconButton :to="KarolWisniewskiCV" label="CV" />
+      <IconButton
+        label="Linkedin"
+        to="https://www.linkedin.com/in/karol-wi%C5%9Bniewski-1a9747200/"
+      />
+      <IconButton label="GitHub" />
+    </div>
+  </div>
+</template>
