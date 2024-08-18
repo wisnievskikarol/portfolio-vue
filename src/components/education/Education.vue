591fb0352e3b44f53ae15640bd83c8006c7f73e7
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-18 11:20:41 +0200

    feat: add Education

---
+++ src/components/education/Education.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Section from '@/components/common/Section.vue'
+
+const educationList = [
+  {
+    institution: 'Łódź University of Technology',
+    degreeDuration: '2019 - 2023',
+    degreeTitle: 'Bachelor of Science in Computer Science',
+    specialization: 'Web Technologies'
+  },
+  {
+    institution: 'ZSMEiE',
+    degreeDuration: '2015 - 2019',
+    degreeTitle: 'IT Technician',
+    specialization: 'Certification of: E12, E13, E14'
+  }
+]
+</script>
+
+<template>
+  <Section title="🎓 education">
+    <EducationItem
+      v-for="(item, index) in educationList"
+      :key="index"
+      :institution="item.institution"
+      :degreeDuration="item.degreeDuration"
+      :degreeTitle="item.degreeTitle"
+      :specialization="item.specialization"
+    />
+  </Section>
+</template>
