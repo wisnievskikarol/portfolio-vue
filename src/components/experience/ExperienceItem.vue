4f97fca3eb68a86b684db8c8861418ce5de1f7d4
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-18 21:54:04 +0200

    feat: add ExperienceItem

---
+++ src/components/experience/ExperienceItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Job } from './types'
+
+defineProps<Job>()
+</script>
+
+<template>
+  <li class="flex gap-1 flex-col">
+    <p class="font-light text-xs">{{ location }}</p>
+    <a :href="companyUrl" target="_blank">
+      <p
+        class="font-medium text-gray underline text-lg transition delay-75 duration-300 ease-in-out hover:text-primary"
+      >
+        {{ companyName }}
+      </p>
+    </a>
+    <p class="font-light text-sm">{{ duration }}</p>
+    <p class="text-sm mt-2 font-normal">{{ position }}</p>
+    <p class="text-sm font-light">
+      {{ description }}
+    </p>
+    <p v-if="techStack" class="text-xs font-light">Tech stack: {{ techStack }}</p>
+  </li>
+</template>
