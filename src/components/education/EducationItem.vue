38d112b97e977cbb2a50e3624a7d15f866e55b2c
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-18 11:21:04 +0200

    feat: add EducationItem

---
+++ src/components/education/EducationItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  institution: string
+  degreeDuration: string
+  specialization?: string
+  degreeTitle: string
+}>()
+</script>
+
+<template>
+  <ul class="text-darkGray border-gray-900 max-w-80 rounded-md">
+    <li class="flex gap-1 flex-col">
+      <p class="font-medium text-gray text-lg">{{ institution }}</p>
+      <p class="font-light text-sm">{{ degreeDuration }}</p>
+      <p class="text-sm mt-2 font-light">{{ degreeTitle }}</p>
+      <p class="text-xs font-light">{{ specialization }}</p>
+    </li>
+  </ul>
+</template>
