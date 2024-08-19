14073b005bf1104dff29faca8435a9152d786799
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-19 20:13:15 +0200

    feat(Section): implement MotionWrapper

--- src/components/common/Section.vue
+++ src/components/common/Section.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
+import MotionWrapper from './MotionWrapper.vue'
 defineProps<{ title: string }>()
 </script>
 
 <template>
-  <div v-bind="$attrs" class="flex gap-4 flex-col">
-    <h2 class="uppercase text-lightGray font-normal text-lg tracking-widest">{{ title }}</h2>
-    <div class="px-2 sm:px-4">
-      <slot></slot>
+  <MotionWrapper>
+    <div v-bind="$attrs" class="flex gap-4 flex-col">
+      <h2 class="uppercase text-lightGray font-normal text-lg tracking-widest">
+        {{ title }}
+      </h2>
+      <div class="px-2 sm:px-4">
+        <slot></slot>
+      </div>
     </div>
-  </div>
+  </MotionWrapper>
 </template>
